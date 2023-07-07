e84d8eca945656d462fb2ccf5c72fc80de788c48
MesserA8 <atihon09@gmail.com>
2023-07-07 18:52:12 +0300

    Amperage screen updated

--- app/src/main/java/com/messer_amd/electrician/MainActivity.kt
+++ app/src/main/java/com/messer_amd/electrician/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.messer_amd.electrician.screens.Amperage
 import com.messer_amd.electrician.ui.theme.ElectricianTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,13 +23,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Amperage()
                 }
             }
         }
     }
 }
-
 
 
 @Preview(showBackground = true)
--- app/src/main/java/com/messer_amd/electrician/screens/Amperage.kt
+++ app/src/main/java/com/messer_amd/electrician/screens/Amperage.kt
@@ -1,23 +1,51 @@
 package com.messer_amd.electrician.screens
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.messer_amd.electrician.R
 
 
-@Preview(showBackground = true, showSystemUi = false)
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun Amperage() {
+    var currentInput by remember { mutableStateOf("") }
+    var resistanceInput by remember { mutableStateOf("") }
+
     Card(
         modifier = Modifier
             .fillMaxSize()
@@ -25,13 +53,132 @@ fun Amperage() {
             .background(Color.LightGray),
         shape = RoundedCornerShape(15.dp)
     ) {
-     Column(
-         modifier = Modifier
-             .padding(10.dp),
-         horizontalAlignment = Alignment.CenterHorizontally,
-         verticalArrangement = Arrangement.Center
-     ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                modifier = Modifier
+                    .padding(top = 16.dp),
+                text = stringResource(R.string.amperage_title),
+                style = MaterialTheme.typography.headlineMedium,
+                color = Color.Black
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Cyan)
+            ) {
+                Row {
+                    Image(
+                        modifier = Modifier
+                            .padding(start = 4.dp)
+                            .size(64.dp),
+                        painter = painterResource(R.drawable.amperage),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(15.dp))
+                    Text(
+                        modifier = Modifier
+                            .padding(4.dp),
+                        text = stringResource(R.string.ohm_law),
+                        style = MaterialTheme.typography.titleSmall
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Row(
+                    modifier = Modifier,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .padding(start = 4.dp),
+                        text = stringResource(R.string.current_text),
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = Color.Black
+                    )
+                    Spacer(modifier = Modifier.width(32.dp))
+                    EditNumberField(
+                        label = R.string.label_volt,
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Next,
+                        ),
+                        value = currentInput,
+                        onValueChanged = { currentInput = it },
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .fillMaxWidth()
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Row(
+                    modifier = Modifier,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .padding(start = 4.dp),
+                        text = stringResource(R.string.resistance_text),
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = Color.Black
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    EditNumberField(
+                        label = R.string.label_ohm,
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Done
+                        ),
+                        value = currentInput,
+                        onValueChanged = { resistanceInput = it },
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .fillMaxWidth()
+                    )
+                }
+            }
 
-     }
+        }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditNumberField(
+    @StringRes label: Int,
+    keyboardOptions: KeyboardOptions,
+    value: String,
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = value,
+        modifier = modifier,
+        onValueChange = onValueChanged,
+        label = {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = stringResource(label))
+            }
+        },
+        keyboardOptions = keyboardOptions
+    )
+}
+
